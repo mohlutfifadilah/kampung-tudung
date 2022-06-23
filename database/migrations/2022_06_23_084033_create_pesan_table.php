ed3b1ba25241cd75a45cee42f49ee38fdd63629a
mohlutfifadilah <mohlutfifadilah23@gmail.com>
2022-06-23 15:49:19 +0700

    create migration and user seed

---
+++ database/migrations/2022_06_23_084033_create_pesan_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePesanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pesan', function (Blueprint $table) {
+            $table->id();
+            $table->integer('ktp');
+            $table->string('nama');
+            $table->string('alamat');
+            $table->integer('nohp');
+            $table->date('tanggal');
+            $table->string('paket');
+            $table->integer('jumlah-orang');
+            $table->string('catatan');
+            $table->integer('total');
+            $table->integer('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pesan');
+    }
+}
