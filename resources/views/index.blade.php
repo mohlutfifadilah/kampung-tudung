d1a350858c5a5fd256a453622354887293332700
mohlutfifadilah <mohlutfifadilah23@gmail.com>
2022-07-12 09:30:12 +0700

    progress user page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,6 +12,23 @@
     <!-- Bulma Version 0.9.0-->
     <link rel="stylesheet" href="https://unpkg.com/bulma@0.9.0/css/bulma.min.css" />
     <link rel="stylesheet" type="text/css" href="{{ asset('css/landing.css') }}">
+    <link rel="stylesheet" href="https://unpkg.com/bulma-modal-fx/dist/css/modal-fx.min.css" />
+    <style>
+        .pointer {
+            cursor: pointer;
+            border: none;
+        }
+
+        .modal-button:hover {
+            filter: blur(1px) brightness(90%);
+            transition: filter 300ms;
+        }
+
+        .modal-content {
+            max-height: none !important;
+            overflow: hidden;
+        }
+    </style>
 </head>
 
 <body>
@@ -41,12 +58,12 @@
                             <a class="navbar-item">
                                 Tentang Kami
                             </a>
-                            <a class="navbar-item">
+                            <a class="navbar-item data-scroll" href="#map">
                                 Lokasi
                             </a>
                             <div class="navbar-item">
                                 <div class="buttons">
-                                    <a class="button is-primary">
+                                    <a class="button is-primary" data-scroll href="#booking">
                                         <strong>Pesan Sekarang</strong>
                                     </a>
                                 </div>
@@ -110,9 +127,65 @@
         </p>
         <div id="bazinga">Bazinga!</div>
     </section>
-    <section class="container booking">
-        <p class="title is-3">Pesan</p>
-        <p class="subtitle has-text-info is-3">Sekarang</p>
+    <section class="container gallery" id="gallery">
+        <div class="columns">
+            <div class="column">
+                <!-- 01 -->
+                <img class="modal-button pointer" data-target="modal-01"
+                    src="https://www.freagraphy.de/wp-content/uploads/2017/10/MG_8072_end-120x130.jpg">
+                <div id="modal-01" class="modal modal-fx-fadeInScale">
+                    <div class="modal-background"></div>
+                    <div class="modal-content">
+                        <img src="https://www.freagraphy.de/wp-content/uploads/2017/10/MG_8072_end.jpg">
+                    </div>
+                    <button class="modal-close is-large" aria-label="close"></button>
+                </div>
+                <div id="modal-01" class="modal modal-fx-fadeInScale"></div>
+            </div>
+        </div>
+    </section>
+    <section class="container video" id="video">
+        <div class="card">
+            <div class="card-content">
+                <div class="columns">
+                    <div class="column">
+                        <p class="title">
+                            Video Profil
+                        </p>
+                        <p class="subtitle has-text-primary">
+                            Kampung Tudung
+                        </p>
+                        <p class="content">
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+                            been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
+                            galley of type and scrambled it to make a type specimen book. It has survived not only five
+                            centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum
+                            passages, and more recently with desktop publishing software like Aldus PageMaker including
+                            versions of Lorem Ipsum.
+                        </p>
+                    </div>
+                    <div class="column">
+                        <iframe width="100%" height="315" src="https://www.youtube.com/embed/WhD9tbrUbnE">
+                        </iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="map mb-5" id="map">
+        <div class="card mb-3">
+            <div class="card-content">
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d63273.376639123!2d109.6116066530762!3d-7.619940037274779!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7acbc66a26ec7b%3A0x75bcd6f02e708e77!2sKampung%20Tudung!5e0!3m2!1sen!2sid!4v1657588390613!5m2!1sen!2sid"
+                    width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </div>
+    </section>
+    <section class="container booking mt-2" id="booking">
+        <p class="title is-3 mt-3">Pesan</p>
+        <p class="subtitle has-text-primary is-3">Sekarang</p>
         <div class="card">
             <div class="card-content">
                 <form action="/send" method="post">
@@ -121,7 +194,8 @@
                         <div class="column">
                             <div class="field">
                                 <div class="control">
-                                    <input class="input" type="text" placeholder="Nama / Instansi" name="nama">
+                                    <input class="input" type="text" placeholder="Nama / Instansi"
+                                        name="nama">
                                 </div>
                             </div>
                         </div>
@@ -197,6 +271,7 @@
     <!-- Get a specific version -->
     <script src="https://cdn.jsdelivr.net/gh/cferdinandi/smooth-scroll@15.0.0/dist/smooth-scroll.polyfills.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/simple-parallax-js@5.5.1/dist/simpleParallax.min.js"></script>
+    <script type="text/javascript" src="https://unpkg.com/bulma-modal-fx/dist/js/modal-fx.min.js"></script>
     <script>
         var scroll = new SmoothScroll('a[href*="#"]', {
             header: '[data-scroll-header]',
