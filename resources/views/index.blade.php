201af6c65bd134558fbe0ceb603b5bafbea86c01
mohlutfifadilah <mohlutfifadilah23@gmail.com>
2022-08-27 23:20:29 +0700

    gallery + paket carousel done

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,7 +15,7 @@
     <link rel="stylesheet" href="https://unpkg.com/bulma-modal-fx/dist/css/modal-fx.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma-carousel@4.0.3/dist/css/bulma-carousel.min.css">
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.0.7/dist/css/splide.min.css">
     <link rel="stylesheet" type="text/css"
         href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
@@ -175,7 +175,7 @@
             </div>
         </div>
     </section>
-    <section class="container gallery my-5" id="gallery">
+    <section class="container gallery splide my-5" id="gallery" aria-labelledby="carousel-heading">
         <div class="text-has-centered mb-3">
             <p class="title">
                 Galeri
@@ -185,16 +185,49 @@
             </p>
             <hr class="is-primary" width="10%">
         </div>
-        <div class="container">
-            <!-- Start Carousel -->
-            <div id="carousel-demo" class="carousel">
+        <div class="splide__track">
+            <ul class="splide__list">
                 @foreach ($gallery as $g)
-                    <img src="{{ asset('storage/' . $g->foto) }}" alt="">
+                    <li class="splide__slide">
+                        <img src="{{ asset('storage/' . $g->foto) }}" alt="">
+                    </li>
                 @endforeach
-            </div>
-            <!-- End Carousel -->
+            </ul>
         </div>
     </section>
+    {{-- <section class="container paket splidee my-5" id="paket" aria-labelledby="carousel-heading">
+        <div class="text-has-centered mb-3">
+            <p class="title">
+                Paket Wisata
+            </p>
+            <p class="subtitle has-text-primary">
+                Kampung Tudung
+            </p>
+            <hr class="is-primary" width="10%">
+        </div>
+        <div class="splide__track">
+            <ul class="splide__list">
+                @foreach ($paket as $g)
+                    <li class="splide__slide">
+                        <div class="card has-text-centered">
+                            <header class="card-header">
+                                <p class="card-header-title is-centered">
+                                    {{ $g->nama }}
+                                </p>
+                            </header>
+                            <div class="card-content">
+                                <div class="content">
+                                    <span class="tag is-medium is-info is-rounded">
+                                        @currency($g->harga)/orang
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+    </section> --}}
     <section class="container paket my-5" id="paket">
         <div class="text-has-centered mb-3">
             <p class="title">
@@ -205,30 +238,26 @@
             </p>
             <hr class="is-primary" width="10%">
         </div>
-        <div class="container">
-            <!-- Start Carousel -->
-            <div id="carousel-demo" class="carousel">
-                @foreach ($paket as $g)
-                    <div class="card">
-                        <div class="card-image">
-                            <figure class="image is-16by9 is-covered">
-                                <img src="https://images.unsplash.com/photo-1550921082-c282cdc432d6?ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80"
-                                    alt="" />
-                            </figure>
-                        </div>
-                        <div class="card-content">
-                            <div class="item__title">
-                                Mon titre 1
-                            </div>
-                            <div class="item__description">
-                                Ici une petite description pour tester le slider
-                            </div>
+        <!-- Start Carousel -->
+        <div id="carousel-demo2" class="carousel">
+            @foreach ($paket as $g)
+                <div class="card has-text-centered">
+                    <header class="card-header">
+                        <p class="card-header-title is-centered">
+                            {{ $g->nama }}
+                        </p>
+                    </header>
+                    <div class="card-content">
+                        <div class="content">
+                            <span class="tag is-medium is-info is-rounded">
+                                @currency($g->harga)/orang
+                            </span>
                         </div>
                     </div>
-                @endforeach
-            </div>
-            <!-- End Carousel -->
+                </div>
+            @endforeach
         </div>
+        <!-- End Carousel -->
     </section>
     <section class="has-text-centered is-fluid mitra my-5 py-5 has-background-primary" id="mitra">
         <div class="container">
@@ -449,6 +478,7 @@
             </p>
         </div>
     </footer>
+
     <script async type="text/javascript" src="{{ asset('js/bulma.js') }}"></script>
     <!-- Always get the latest version -->
     <!-- Not recommended for production sites! -->
@@ -465,6 +495,7 @@
     <script src="https://cdn.jsdelivr.net/npm/simple-parallax-js@5.5.1/dist/simpleParallax.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bulma-carousel@4.0.3/dist/js/bulma-carousel.min.js"></script>
     <script type="text/javascript" src="https://unpkg.com/bulma-modal-fx/dist/js/modal-fx.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.0.7/dist/js/splide.min.js"></script>
     <script>
         var scroll = new SmoothScroll('a[href*="#"]', {
             header: '[data-scroll-header]',
@@ -483,10 +514,25 @@
             slidesToShow: 3,
             infinite: true,
         });
+        bulmaCarousel.attach('#carousel-demo2', {
+            slidesToScroll: 1,
+            slidesToShow: 4,
+            infinite: true,
+        });
 
         $('#number_only').bind('keyup paste', function() {
             this.value = this.value.replace(/[^0-9]/g, '');
         });
+
+        new Splide('#gallery', {
+            type: 'loop',
+            padding: '5rem',
+            perPage: 3,
+            perMove: 1,
+            focus: 'center',
+            autoWidth: true,
+            autoHeight: true
+        }).mount();
     </script>
 </body>
 
